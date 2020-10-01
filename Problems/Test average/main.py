018665a768d2f203f4e26aa0db96ffab6472147c
Didi <ofisser86@gmail.com>
2020-10-01 17:56:10 +0300

    Start implement

---
+++ Problems/Test average/main.py
@@ -0,0 +1,4 @@
+def average_mark(*args):
+    print(args)
+    return round(sum(args) / len(args), 1)
+
--- Tic-Tac-Toe with AI/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe with AI/task/tictactoe/tictactoe.py
@@ -1,9 +1,15 @@
+import random
+
+
 class TicTacToe:
     def __init__(self):
-        self.board = None
         # Create empty board when the game starts
+        self.board = None
         self.create_board([' ' for _ in range(9)])
         # Store current move ('X' or 'O') turn to action
+        self.menu()
+        self.player1 = None
+        self.player2 = None
         self.current_move = None
 
     def create_board(self, empty_cells):
@@ -12,8 +18,6 @@ class TicTacToe:
                  (1, 2), empty_cells[3], (2, 2), empty_cells[4], (3, 2), empty_cells[5],
                  (1, 1), empty_cells[6], (2, 1), empty_cells[7], (3, 1), empty_cells[8]]
         self.board = board
-        self.draw_board()
-        self.menu()
 
     def draw_board(self):
         print(f"""---------
@@ -23,23 +27,20 @@ class TicTacToe:
 ---------""")
 
     # Check if entered coordinate is right
-    # Todo: Improve DRY
     def check_coord(self, x, y):
+        msg = None
         if x.isalpha() or y.isalpha():
-            print('You should enter numbers!')
-            self.menu()
+            msg = 'You should enter numbers!'
         elif int(x) > 3 or int(y) > 3:
-            print('Coordinates should be from 1 to 3!')
-            self.menu()
+            msg = 'Coordinates should be from 1 to 3!'
         elif self.board[self.board.index((int(x), int(y))) + 1] != ' ':
-            print("This cell is occupied! Choose another one!")
-            self.menu()
+            msg = "This cell is occupied! Choose another one!"
+        return msg
 
     def check_position(self, x, y):
-
-        self.board[self.board.index((x, y)) + 1] = "X"
-        self.current_move = 'X'
+        msg = None
         # All winning combinations
+
         if self.board[1] == self.board[3] == self.board[5] != ' ' or \
                 self.board[7] == self.board[9] == self.board[11] != ' ' or \
                 self.board[13] == self.board[15] == self.board[17] != ' ' or \
@@ -48,37 +49,86 @@ class TicTacToe:
                 self.board[5] == self.board[11] == self.board[17] != ' ' or \
                 self.board[1] == self.board[9] == self.board[17] != ' ' or \
                 self.board[5] == self.board[9] == self.board[13] != ' ':
-            self.draw_board()
-            print(f'{self.current_move} wins')
+            msg = f'{self.current_move} wins'
         elif ' ' not in self.board:
-            self.draw_board()
-            print('Draw')
-        else:
-            self.draw_board()
-            print('Making move level "easy"')
-            # PC making a move
-            self.easy_level()
-            self.current_move = 'O'
-
-        quit()
+            msg = 'Draw'
+        return msg
 
     # Easy game level - PC choose first empty cell (cell with "<space>")
     def easy_level(self):
         for i in self.board:
             if i == ' ':
-                self.board[self.board.index(i)] = 'O'
+                self.board[self.board.index(i)] = self.current_move
+                print('Making move level "easy"')
                 self.draw_board()
+                self.current_move = 'O'
+                self.play_with_easy_pc()
+
+    def make_move(self, x, y):
+        self.board[self.board.index((x, y)) + 1] = self.current_move
+
+    def play_with_easy_pc(self):
+
+        coord = input("Enter the coordinates:").split()
+        x, y = coord
+
+        if self.check_coord(x, y) is None:
+            if self.check_position(int(x), int(y)) is None:
+                self.make_move(int(x), int(y))
+                self.draw_board()
+                self.current_move = 'X'
+                self.easy_level()
+            else:
+                print(self.check_position(int(x), int(y)))
                 self.menu()
+        else:
+            print(self.check_coord(x, y))
+            self.play_with_easy_pc()
+
+    def user_user(self):
+        ...
+
+    def easy_easy(self):
+        x = random.randint(1, 3)
+        y = random.randint(1, 3)
+
+        if self.current_move == 'X':
+            self.current_move = 'O'
+        else:
+            self.current_move = 'X'
+
+        if self.check_coord(str(x), str(y)) is None:
+            if self.check_position(x, y) is None:
+                self.draw_board()
+                self.check_position(x, y)
+                self.board[self.board.index((x, y)) + 1] = self.current_move
+                print('Making move level "easy"')
+                self.easy_easy()
+            else:
+                self.draw_board()
+                print(self.check_position(x, y))
+                self.menu()
+        else:
+            self.easy_easy()
 
     def menu(self):
-        coord = input("Enter the coordinates:").split()
-        try:
-            x, y = coord
-            self.check_coord(x, y)
-            self.check_position(int(x), int(y))
-        except ValueError:
-            print('You should enter numbers!')
-            self.menu()
+        while True:
+            command = input("Input command:").split()
+            if command[0] == 'exit' and len(command) == 1:
+                quit()
+            elif len(command) != 3 or command[0] != 'start':
+                print("Bad parameters!")
+            elif command == ['start', 'easy', 'user']:
+                self.draw_board()
+                self.player1, self.player2 = command[1], command[2]
+                self.current_move = 'X'
+                self.easy_level()
+            elif command == ['start', 'easy', 'easy']:
+                self.player1, self.player2 = command[1], command[2]
+                self.current_move = 'O'
+                self.easy_easy()
+            elif command == ['start', 'user', 'user']:
+                ...
 
 
 if __name__ == '__main__':
